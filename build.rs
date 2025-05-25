15a56c6eb1b59b2066629884be907d212029c67f
Peter Jin <>
2025-05-24 19:36:57 -0700

    Update 2025-05-24-1. Fix build as submodule.

--- build.rs
+++ build.rs
@@ -1,13 +1,16 @@
+use std::path::{PathBuf};
+
 fn main() {
-  println!("cargo:rerun-if-changed=build.rs");
+  let manifest_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+  println!("cargo:rerun-if-changed={}/build.rs", manifest_dir.display());
   // TODO: only pre-build is currently supported.
-  println!("cargo:rerun-if-changed=_build/include/uv.h");
-  println!("cargo:rerun-if-changed=_build/include/uv/darwin.h");
-  println!("cargo:rerun-if-changed=_build/include/uv/errno.h");
-  println!("cargo:rerun-if-changed=_build/include/uv/threadpool.h");
-  println!("cargo:rerun-if-changed=_build/include/uv/unix.h");
-  println!("cargo:rerun-if-changed=_build/include/uv/version.h");
-  println!("cargo:rerun-if-changed=_build/lib/libuv.a");
-  println!("cargo:rustc-link-search=native=_build/lib");
+  println!("cargo:rerun-if-changed={}/_build/include/uv.h", manifest_dir.display());
+  println!("cargo:rerun-if-changed={}/_build/include/uv/darwin.h", manifest_dir.display());
+  println!("cargo:rerun-if-changed={}/_build/include/uv/errno.h", manifest_dir.display());
+  println!("cargo:rerun-if-changed={}/_build/include/uv/threadpool.h", manifest_dir.display());
+  println!("cargo:rerun-if-changed={}/_build/include/uv/unix.h", manifest_dir.display());
+  println!("cargo:rerun-if-changed={}/_build/include/uv/version.h", manifest_dir.display());
+  println!("cargo:rerun-if-changed={}/_build/lib/libuv.a", manifest_dir.display());
+  println!("cargo:rustc-link-search=native={}/_build/lib", manifest_dir.display());
   println!("cargo:rustc-link-lib=static=uv");
 }
